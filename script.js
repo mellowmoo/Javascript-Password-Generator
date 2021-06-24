b3bf2e78bdefffdb4ab079de7c825567e9980d9f
mellowmoo <84346437+mellowmoo@users.noreply.github.com>
2021-06-23 22:28:49 -0400

    Nearly finalized, website working as intended, no errors, need to update the readme to look more professional

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var uUppercase;
 var uLowercase;
 
 // Password Variable Arrays
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 // alpha2 variable for use in the loop for uppercase conversion
@@ -33,6 +33,11 @@ function writePassword() {
 // uppercase conversion function
 function toUpper(x) {
   return x.toUpperCase();
+}
+
+// Function to insert the final password into the textbox to display on the page
+function passInsert(finalPass) {
+  document.getElementById("password").textContent = finalPass;
 }
 
 // Generate Password Function
@@ -75,8 +80,51 @@ function generatePassword() {
   else if (uNumber && uLowercase && uUppercase) {
     uChoice = number.concat(alpha, alpha2);
   }
-
+  // checking for 2 positive choices
+  else if (uCharacter && uNumber) {
+    uChoice = character.concat(number);
+  }
+  else if (uCharacter && uLowercase) {
+    uChoice = character.concat(alpha);
+  }
+  else if (uCharacter && uUppercase) {
+    uChoice = character.concat(alpha2);
+  }
+  else if (uLowercase && uNumber) {
+    uChoice = alpha.concat(number);
+  }
+  else if (uLowercase && uUppercase) {
+    uChoice = alpha.concat(alpha2);
+  }
+  else if (uNumber && uUppercase) {
+    uChoice = number.concat(alpha2);
+  }
+  // checking for 1 positive choice
+  else if (uCharacter) {
+    uChoice = character;
+  }
+  else if (uNumber) {
+    uChoice = number;
+  }
+  else if (uLowercase) {
+    uChoice = alpha;
+  }
+  else if (uUppercase) {
+    uChoice = alpha2;
+  }
+  // Declaration of placeholder variable for password as an ARRAY 
+  var password = [];
+  // Random selecttion of variables to place into password
+  for (i = 0; i < passSize; i++) {
+    var processedChoices = uChoice[Math.floor(Math.random() * uChoice.length)];
+    password.push(processedChoices);
+  }
+  // converting the password array to a string
+  var finalPass = password.join("");
+  passInsert(finalPass);
+  return finalPass;
+  
 }
 
-// TODO : else if for 4 negative inputs by the user, if statement that determines the choices made by the user, else if for all positive choices
+// TODO: Maybe add a copy to clipboard function
 
